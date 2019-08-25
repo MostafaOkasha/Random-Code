bb49edc030c4d1defedbcdf857fa4ad5045f2344
Mostafa Okasha <okashm@mcmaster.ca>
2019-08-25 04:35:42 -0400

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,57 @@
+def quicksort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return quicksort(less)+equal+quicksort(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to hande the part at the end of the recursion -
+           #when you only have one element in your array, just return the array.
+        return array
+
+array=[12,4,5,6,7,3,1,15]
+sortedarray = quicksort(array)
+print(sortedarray)
+
+
+def quicksort(array):
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        return quicksort(less) + (equal) + quicksort(greater)
+    else: #you only have one element
+        return array
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
